ebe23a1f4e9dbbee7832b4532a31b31716d043b8
hernan840 <hernanflores21@gmail.com>
2023-02-12 03:48:11 -0300

    dar fucnionalidad de mostrar y ocultar forulario

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/service/ui.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -14,7 +16,15 @@ export class AddTaskComponent implements OnInit {
   text: string = "";
   day: string = "";
   reminder: boolean = false;
-  constructor() { }
+
+  showAddTask: boolean = false;
+  subscription?: Subscription
+
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle()
+    .subscribe((value) => this.showAddTask = value)
+   }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/service/ui.service';
+
+//UiService se  usa para poder escuchar cunado se hace click en el boton
+//al otro componente
 
 @Component({
   selector: 'app-header',
@@ -6,15 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
-  title:string = 'My Task List';
 
-  constructor(){}
+  title:string = 'My Task List';
+  showAddTask:boolean = true;
+  subscription?: Subscription;
+
+
+  constructor(private uiService:UiService){
+    //para que cambie el btn al precionar 
+    this.subscription = this.uiService.onToggle().subscribe((value) => 
+              this.showAddTask = value)
+  }
   ngOnInit(): void {
     
   }
 
   toggelAddTask(){
-    console.log("toggelAddTask!!");
+    // console.log("toggelAddTask!!");
+    this.uiService.toggleAddTask(); //para que cambie el valor del toggel
   }
 
 }
